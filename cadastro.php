869eaeb139f0ff9bcd55d9686a95fde2fa9a6d9f
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-10-14 17:34:34 -0300

    Página cadastro

--- cadastro.php
+++ cadastro.php
@@ -42,40 +42,40 @@
 
     <div class="container">
 
+        <img src="http://purepng.com/public/uploads/large/51502306185qgm7goy4yxccepo38n6v8pmwrmttpahzenrfznpugnaymrqgz3s38rsftxrspwpxlrrwobw2g4gx5crjne3nucymoydjscwfatp0.png" alt="Cute Cat" class="cat-left">
+
         <form action="sistema/recebemensagem.php" method="post" id="formul">
 
-            <input type="text" name="txtnome" id="txtnome" placeholder="Nome Completo" class="field long-field" value="<?php echo $nome; ?>"></br>
+            <div class="radio-btns">
+                <label for="cpf">Pessoa Física</label>
+                <input type="radio" id="pf" name="pessoa" value="pf">
+                <input type="radio" id="pj" name="pessoa" value="pj">
+                <label for="cnpj">Pessoa Jurídica</label>
+            </div>            
 
-            <input type="email" name="txtemail" placeholder="E-mail" class="field long-field" value="<?php echo $email; ?>"></br>
+            <div class="general">                
+                <input type="text" name="txtendereco" id="txtendereco" placeholder="Endereço" value="<?php echo $endereco; ?>" class="field long-field" required></br>
+                <input type="email" name="txtemail" placeholder="E-mail" class="field long-field" value="<?php echo $email; ?>" required></br>
+                <input type="tel" name="txttelefone" id="txttelefone" maxlength="11" class="field long-field phone_with_ddd" placeholder="Telefone" value="<?php echo $telefone; ?>" required></br>
+            </div>
 
-            <input type="text" name="date" id="txtdata" placeholder="Data de Nascimento" maxlength="11" class="field short-field date" value="<?php echo $data; ?>">
+            <div class="content-cpf">
+                <input type="text" name="txtnome" id="txtnome" placeholder="Nome Completo" class="field long-field" value="<?php echo $nome; ?>" required></br> 
+                <input type="text" name="date" id="txtdata" placeholder="Data de Nascimento" maxlength="11" class="field short-field date" value="<?php echo $data; ?>" required>            
+                <input type="text" name="txtcpf" id="txtcpf" placeholder="CPF" maxlength="14" class="field short-field cpf" value="<?php echo $codigocpf; ?>" required></br>
+            </div>
 
-            <input type="tel" name="txttelefone" id="txttelefone" maxlength="11" class="field short-field phone_with_ddd" placeholder="Telefone" value="<?php echo $telefone; ?>"></br>
+            <div class="content-cnpj">
+                <input type="text" name="txtrazao" id="txtrazao" placeholder="Razão Social" value="<?php echo $razao; ?>" class="field long-field" required></br>
+                <input type="text" name="txtfantasia" id="txtfantasia" placeholder="Nome Fantasia" value="<?php echo $fantasia; ?>" class="field long-field" required></br>
+                <input type="text" name="txtcnpj" id="txtcnpj" placeholder="CNPJ" value="<?php echo $codigocnpj; ?>" class="field short-field cnpj" required></br>
+            </div>
 
-            <input type="text" name="txtendereco" id="txtendereco" placeholder="Endereço" value="<?php echo $endereco; ?>" class="field long-field"></br>
-
-
-            <label for="cpf">Pessoa Física</label>
-
-            <input type="radio" id="pf" name="pessoa" value="pf">
+            <input type="submit" value="Enviar" class="animate-up">
             
-            <input type="radio" id="pj" name="pessoa" value="pj">
-            
-            <label for="cnpj">Pessoa Jurídica</label></br>
-
-
-            <input type="text" name="txtcpf" id="txtcpf" placeholder="CPF" maxlength="14" class="field short-field cpf" value="<?php echo $codigocpf; ?>"></br>
-
-            <input type="text" name="txtcnpj" id="txtcnpj" placeholder="CNPJ" value="<?php echo $codigocnpj; ?>" class="field short-field"></br>
-
-
-            <input type="text" name="txtrazao" id="txtrazao" placeholder="Razão Social" value="<?php echo $razao; ?>" class="field long-field"></br>
-
-            <input type="text" name="txtfantasia" id="txtfantasia" placeholder="Nome Fantasia" value="<?php echo $fantasia; ?>" class="field long-field"></br>
-
-
-            <input type="submit" value="Enviar">
         </form>
+
+        <img src="http://purepng.com/public/uploads/large/purepng.com-sitting-catcatcatsanimalspetscute-cat-25152016380020k8o.png" alt="Cute Cat" class="cat-right">
 
     </div>
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,22 @@
 $(document).ready(function(){
-  $("#txtcnpj").hide();
-  $("#txtrazao").hide();
-  $("#txtfantasia").hide();  
-  $("#txtcpf").hide();  
+  $(".content-cpf").hide();
+  $(".content-cnpj").hide();
+
   $('input[type="radio"]').click(function(event){
     if ($("#pf").prop("checked"))
     {             
-      $("#txtcnpj").hide();
-      $("#txtrazao").hide();
-      $("#txtfantasia").hide();
-      $("#txtcpf").show();     
+      $(".content-cnpj").hide();
+      $(".content-cpf").show();
     }
     else if ($("#pj").prop("checked"))
     {
-      $("#txtcpf").hide();     
-      $("#txtcnpj").show();
-      $("#txtrazao").show();
-      $("#txtfantasia").show();             
+      $(".content-cpf").hide();
+      $(".content-cnpj").show();
     }
     });
 
     $('.date').mask('00/00/0000');
-      $('.phone_with_ddd').mask('(00) 00000-0000');
-      $('.cpf').mask('000.000.000-00', {reverse: true});
-
-      $("#formul").validate({
-        rules : {
-          txtnome:{
-            required:true,
-            minlength:5
-          },
-          txtemail:{
-            required:true,
-            minlength:7,
-          },
-          txtcpf: {
-            required:true
-          },
-          txtdata:{
-            required:true
-          },
-          txttelefone: {
-            required:true
-          }
-        },
-        messages:{
-          txtnome:{
-            required:"Por favor, informe seu nome",
-            minlength:"O nome deve ter pelo menos 5 caracteres"
-          },
-          txtemail:{
-            required:"Por favor, informe um e-mail",
-            minlength:"Informe um e-mail válido"
-          },
-          txtcpf: {
-            required:"Informe seu CPF"
-          },
-          txtdata:{
-            required:"Informe data de nascimento"
-          },
-          txttelefone: {
-            required:"Por favor, informe seu celular"
-          }
-        }
-      });
+    $('.phone_with_ddd').mask('(00) 00000-0000');
+    $('.cpf').mask('000.000.000-00', {reverse: true});    
+    $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
 });
